619870310998b9fe94119f2daf10521d3c9d1073
martingimenez7 <47359197+martingimenez7@users.noreply.github.com>
2019-03-26 10:29:46 -0300

    Add files via upload

---
+++ recursividad/recursividad.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include"recursividad.h"
+
+int factorial(int numero)
+{
+    int resultado;
+
+    if(numero == 0 || numero == 1){
+        resultado = 1;
+    }else{
+           resultado = numero * factorial(numero - 1);
+         }
+
+    return resultado;
+}
