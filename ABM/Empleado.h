1fef7537976c29c92e9ac5614d1f704125e70f4b
martingimenez7 <47359197+martingimenez7@users.noreply.github.com>
2019-04-16 11:59:03 -0300

    Add files via upload

---
+++ ABM/Empleado.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+#define LIBRE 0
+#define OCUPADO 1
+
+typedef struct
+{
+    int legajo;
+    char nombre[20];
+    char sexo;
+    float sueldoBruto;
+    float sueldoNeto;
+    int estado;
+} eEmpleado;
+
+void mostrarEmpleado(eEmpleado);
+void cargarEmpleado(eEmpleado[], int);
+void mostrarListaEmpleados(eEmpleado[], int);
+
+int buscarLibre(eEmpleado[], int);
+void inicializarEmpleados(eEmpleado[], int);
+void hardcodearDatosEmpleados(eEmpleado[], int);
+
+int modificar(eEmpleado[],int);
